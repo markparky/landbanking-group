93953009c0af6a5c67845f3302b64d4e5dd406d7
marks <mark.steinbuch@momentum.co.za>
2024-10-31 20:31:09 +0200

    testing api and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import { FaSearch } from "react-icons/fa";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const options = {
+  method: "GET",
+  headers: { "x-api-key": "5oO8ABJ4BAYYnfjb1BhBDA==JuwALhZExV7jSzU0" },
+};
+
+const App = () => {
+  const searchQuery = useRef();
+  const [enteredSearchQuery, setEnteredSearchQuery] = useState('');
+
+  const fetchData = async () => {
+    if(enteredSearchQuery.length>0){
+      const response = await fetch(
+        `https://api.api-ninjas.com/v1/animals?name=${enteredSearchQuery}`,
+        options
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    }else{
+      return [];
+    }
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [enteredSearchQuery],
+    queryFn: fetchData,
+    cacheTime: 60000,
+    staleTime: 30000,
+    refetchInterval: 120000,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
+
+  function handleClick() {
+    setEnteredSearchQuery(searchQuery.current.value);
+    searchQuery.current.value = "";
+  }
 
   return (
     <>
-      <div className='bg-red-500'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-sm min-w-[200px]">
+        <div className="relative">
+          <input
+            ref={searchQuery}
+            className="w-full placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            placeholder="Find your wildlife..."
+          />
+          <button
+            className="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+            onClick={handleClick}
+          >
+            <FaSearch id="search-icon" />
+            Search
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>
+            <h1>{item.name}</h1>
+            <h2>{item.locations[0]}</h2>
+            <p>{item.characteristics.most_distinctive_feature}</p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
